51849a3cc9138524a512e75534de4fece2fd7f8f
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-24 17:17:34 +0900

    update/refactor contract.test.ts - use parameterized tests

--- src/__test__/contract.test.ts
+++ src/__test__/contract.test.ts
@@ -1,6 +1,8 @@
 import { Contract } from "../contract";
 import { MSWord } from "../ms_word";
 import { MSExcel } from "../ms_excel";
+import { Ichitaro } from "../ichitaro";
+import { Sanshiro } from "../sanshiro";
 
 function useFactory() {
   const createContract = (
@@ -13,13 +15,57 @@ function useFactory() {
 }
 describe('契約モデル', () => {
   describe("は製品を一つ保有する", () => {
-    test('MS Wordを購入した場合、Contract.product = MSWord となること', () => {
-      const contract = useFactory().createContract({product: new MSWord()});
-      expect("MS Word").toEqual(contract.product.name);
+    test.each([
+      ["MS Word", new MSWord()],
+      ["MS Excel", new MSExcel()],
+      ["一太郎", new Ichitaro()],
+      ["三四郎", new Sanshiro()],
+    ])('%sを購入した場合、Contract.product = "%s" となること', (name, product) => {
+      const contract = useFactory().createContract({product});
+      expect(name).toEqual(contract.product.name);
     });
-    test('MS Excelを購入した場合、Contract.product = MS Excel となること', () => {
-      const contract = useFactory().createContract({product: new MSExcel()})
-      expect("MS Excel").toEqual(contract.product.name);
+  });
+  describe("は契約日を保有する", () => {
+    test.each`
+      signedDate | description
+      ${"19700101"} | ${"過去日"}
+      ${"20210521"} | ${"現在日"}
+      ${"20991231"} | ${"未来日"}
+    `('契約日が$descriptionである"%signedDate"の場合は、"%signedDate"を返却する', ({signedDate, description}) => {
+      const contract = useFactory().createContract({signedDate});
+      expect(signedDate).toEqual(contract.signedDate);
     });
-  })
+  });
+  describe("は売上を返却する", () => {
+    test.each`
+      product | name | price
+      ${new MSWord()} | ${"MS Word"} | ${new MSWord().price}
+      ${new MSExcel()} | ${"MS Excel"} | ${new MSExcel().price}
+      ${new Ichitaro()} | ${"一太郎"} | ${new Ichitaro().price}
+      ${new Sanshiro()} | ${"三四郎"} | ${new Sanshiro().price}
+    `("$nameの場合、売上が$priceであること", ({product, price}) => {
+      const contract = useFactory().createContract({product});
+      expect(price).toEqual(contract.revenue);
+    });
+  });
+/*
+  describe("契約成立をしたら、収益認識を返却する", () => {
+    test("ワードプロセッサの場合", () => {
+      const product = new MSWord();
+      const contract = useFactory().createContract({product});
+      const results = contract.signed();
+      expect(18800).toEqual(results[0].amount);
+      expect("20210524").toEqual(results[0].date);
+    });
+    test("スプレッドシートの場合", () => {
+      const product = new MSExcel();
+      const contract = useFactory().createContract({product});
+      const results = contract.signed();
+      expect(27800 * 2/3).toEqual(results[0].amount);
+      expect("20210524").toEqual(results[0].date);
+      expect(27800 * 1/3).toEqual(results[1].amount);
+      expect("20210624").toEqual(results[1].date);
+    });
+  });
+*/
 });
--- src/contract.ts
+++ src/contract.ts
@@ -1,4 +1,4 @@
-import { Product } from "./product";
+import { Category, Product } from "./product";
 import { RevenueRecognition } from "./revenue_recognition"
 
 export type ContractType = {
@@ -14,9 +14,58 @@ export class Contract {
   revenueRecognitions: RevenueRecognition[]
 
   constructor({product, signedDate}: Pick<ContractType, "product" | "signedDate">) {
+    // this.purchase({product, signedDate});
     this.product = product;
     this.signedDate = signedDate;
-    this.revenue = 0; // TODO
+    this.revenue = product.price;
+    this.revenueRecognitions = [];
+    this.generateRevenueRecognitions(product);
+  }
+
+  private generateRevenueRecognitions(product: Product) {
+    switch(product.category) {
+      case Category.WordProcessor:
+        this.revenueRecognitions = [
+          new RevenueRecognition({
+            date: this.signedDate,
+            amount: this.product.price
+          })
+        ];
+        break;
+      case Category.SpreadSheet:
+        this.revenueRecognitions = [
+          new RevenueRecognition({
+            date: this.signedDate,
+            amount: this.product.price * 2/3
+          }),
+          new RevenueRecognition({
+            date: this.signedDate,
+            amount: this.product.price * 1/3
+          })
+        ];
+        break;
+      default:
+        this.revenueRecognitions = [];
+        break;
+    }
+  }
+
+  signed(): RevenueRecognition[] {
+    return this.revenueRecognitions;
+  }
+
+  validate(): boolean {
+    const amounts = this.revenueRecognitions.reduce((sum, model) => sum + model.amount, 0)
+    return this.revenue === amounts;
+  }
+
+/*
+  // 配列化対応用
+  purchase({product, signedDate}: Pick<ContractType, "product" | "signedDate">) {
+    this.product = product; // TODO: 配列にしたい
+    this.signedDate = signedDate; // TODO: YYYYMMDD以外弾きたい
+    this.revenue = product.price;
     this.revenueRecognitions = []; // TODO
   }
+*/
 }
