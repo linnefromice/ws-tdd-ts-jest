3603b8544036da6b0b9f6aa530beae320dfe7807
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-24 14:20:28 +0900

    refactor - create factory models method (with default values)

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -1,6 +1,5 @@
 import { Product, ProductType } from "../product";
 
-/*
 function useFactory() {
   const createProduct = (
     {name = "Default Name", category = "Default Category", price = 0}
@@ -10,30 +9,24 @@ function useFactory() {
     createProduct: createProduct
   }
 }
-*/
 
 describe('製品', () => {
   describe("は名前を持つ", () => {
     test('名前"製品名XYZ"の製品は、Product#nameで"製品名XYZ"を返す', () => {
-      const product = new Product({name: "製品名XYZ", category: "", price: 0})
-      // const product = useFactory().createProduct({name: "製品名XYZ"})
+      const product = useFactory().createProduct({name: "製品名XYZ"})
       expect("製品名XYZ").toEqual(product.name);
     });
   });
   describe("はカテゴリを持つ", () => {
     test('カテゴリ"ワードプロセッサ"の製品は、Product#categoryで"ワードプロセッサ"を返す', () => {
-      const product = new Product({category: "ワードプロセッサ", name: "", price: 0})
-      // const product = useFactory().createProduct({name: "ワードプロセッサ"})
+      const product = useFactory().createProduct({name: "ワードプロセッサ"})
       expect("ワードプロセッサ").toEqual(product.category);
     });
   });
   describe("はカテゴリを持つ", () => {
     test('価格"10,000円"の製品は、Product#priceで"10000"を返す', () => {
-      const product = new Product({price: 10000, name: "", category: ""})
-      // const product = useFactory().createProduct({price: 10000})
+      const product = useFactory().createProduct({price: 10000})
       expect(10000).toEqual(product.price);
     });
   });
 });
-
-
