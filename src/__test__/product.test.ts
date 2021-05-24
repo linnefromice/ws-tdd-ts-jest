6b96654b485e44a0fce97051747fe99e0ffb95c1
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-24 14:51:52 +0900

    case category

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -1,8 +1,8 @@
-import { Product, ProductType } from "../product";
+import { Product, Category } from "../product";
 
 function useFactory() {
   const createProduct = (
-    {name = "Default Name", category = "Default Category", price = 0}
+    {name = "Default Name", category = Category.WordProcessor, price = 0}
   ): Product => new Product({name: name, category: category, price: price});
 
   return {
@@ -10,7 +10,7 @@ function useFactory() {
   }
 }
 
-describe('製品', () => {
+describe('製品モデル', () => {
   describe("は名前を持つ", () => {
     test('名前"製品名XYZ"の製品は、Product#nameで"製品名XYZ"を返す', () => {
       const product = useFactory().createProduct({name: "製品名XYZ"})
@@ -19,14 +19,22 @@ describe('製品', () => {
   });
   describe("はカテゴリを持つ", () => {
     test('カテゴリ"ワードプロセッサ"の製品は、Product#categoryで"ワードプロセッサ"を返す', () => {
-      const product = useFactory().createProduct({name: "ワードプロセッサ"})
+      const product = useFactory().createProduct({category: Category.WordProcessor})
       expect("ワードプロセッサ").toEqual(product.category);
     });
+    test('カテゴリ"スプレッドシート"の製品は、Product#categoryで"スプレッドシート"を返す', () => {
+      const product = useFactory().createProduct({category: Category.SpreadSheet})
+      expect("スプレッドシート").toEqual(product.category);
+    });
+    test.skip('カテゴリはワードプロセッサ/スプレッドシート以外は含められない', () => {
+      // TODO: PowerPointのパターン
+    });
   });
-  describe("はカテゴリを持つ", () => {
+  describe("は価格を持つ", () => {
     test('価格"10,000円"の製品は、Product#priceで"10000"を返す', () => {
       const product = useFactory().createProduct({price: 10000})
       expect(10000).toEqual(product.price);
     });
   });
 });
+
--- src/product.ts
+++ src/product.ts
@@ -1,12 +1,17 @@
+// type Category = "ワードプロセッサ" | "スプレッドシート"
+export enum Category {
+  WordProcessor = "ワードプロセッサ",
+  SpreadSheet = "スプレッドシート"
+}
 export type ProductType = {
   name: string;
-  category: string;
+  category: Category;
   price: number;
 }
 
 export class Product {
   name: string;
-  category: string;
+  category: Category;
   price: number;
 
   constructor({name, category, price}: ProductType) {
