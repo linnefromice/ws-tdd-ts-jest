2be09a75bdb64b8053b3d4d5696b7499ea399276
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-26 00:07:18 +0900

    refactor

--- src/__test__/revenue_recognition.test.ts
+++ src/__test__/revenue_recognition.test.ts
@@ -1,12 +1,23 @@
 import { RevenueRecognition } from "../revenue_recognition"
 
+function useFactory() {
+  const createRevenueRecognition = (
+    {date = "1970-01-01", amount = 0}
+  ): RevenueRecognition => new RevenueRecognition({date: date, amount: amount});
+
+  return {
+    createRevenueRecognition: createRevenueRecognition
+  }
+}
 describe('収益認識', () => {
   test('は日付を保有する', () => {
-    const revenueRecognition = new RevenueRecognition({date: "20210524", amount: 10000});
-    expect("20210524").toEqual(revenueRecognition.date);
+    const date = "2021-05-24";
+    const revenueRecognition = useFactory().createRevenueRecognition({date});
+    expect(date).toEqual(revenueRecognition.date);
   });
   test('は金額を保有する', () => {
-    const revenueRecognition = new RevenueRecognition({date: "20210524", amount: 10000});
-    expect(10000).toEqual(revenueRecognition.amount);
+    const amount = 10000;
+    const revenueRecognition = useFactory().createRevenueRecognition({amount});
+    expect(amount).toEqual(revenueRecognition.amount);
   });
 });
